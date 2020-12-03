898a4c22c618f8f25bf09cf33061c50eb8c46768
melouellette <klok@flemingcollege.ca>
2020-12-03 14:32:47 -0500

    user inputs

---
+++ Kendrick.py
@@ -0,0 +1,33 @@
+input_PreferenceList = [] # Create empty user preference list 
+
+# Users input their preferences in the PreferenceList
+
+# Users' preference on their starting point
+startingpoint = str(input("Where would you like your starting point be? E = East Gate and W West Gate"))
+
+# Users' preferences on their distance from the starting point
+distgate = int(input("How far would you want to travel to your camp ground? Under 20km: enter 1, \
+                          Between 21 - 40km: enter 2, Over 40km: enter 3 "))
+
+# Users' preferences on electric campsites
+electricCampsite = int(input("Would you want your camp ground that has electric camp sites? Yes: enter 1, \
+                              No: enter 0"))
+
+# Users' preferences on boat ramp
+boatramp = int(input("Would you want the lakes in your camp ground that allows motor boat? Yes: enter 1, \
+                      No: enter 0"))
+
+# Users' preference on the distance to visitor centre
+proxvisit = int(input("How far would you like to have the Visitor Centre nearby? Under 20km: enter 1, \
+                       Between 20 - 40km: enter 2, over 40km: enter 3"))
+
+# Users' preference on the trails' difficulty
+trailpref = int(input("What trail difficulty would you like to have in your camp ground? Easy: enter 1, \
+                       Moderate: enter 2, Hard: enter 3"))
+
+# Users' preference on the distance to trailer station
+trailerstation = int(input("how far would you want a trailer station near your camp ground? \
+                            Under 10km: enter 1, Between 11 - 20km: enter 2, over 21km: enter 3"))
+                    
+
+
