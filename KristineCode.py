93f3bfa00450fd7654c6d9ec126bf798ac2320f5
kluangkhflemingcollegec <kluangkh@flemingcollege.ca>
2020-12-03 13:49:01 -0500

    Kristine copy of code

---
+++ KristineCode.py
@@ -0,0 +1,22 @@
+# GEOM 67 Group Project 
+# Name of Program: Alongquin Park Campground Selector
+# Authors: Kristine Luangkhot, James Serendip, Kathryn Little, Kendrick Lok 
+# Date last modified: 
+# Program Purpose: to conduct a site suitability analysis for a campground in Algonquin Provincial Park along the highway 60 corridor
+
+# Program Use: This program will be used by people looking to select a campground in Algonquin Park based on specific ranked criteria
+
+# Program Structure: 
+
+# Assumptions made: This program assumes that the user is only looking to book a campground along the Highway 60 corridor of Algonquin
+# Provincial Park in Ontario. It also assumes that elevation and ground level are not a factor, given these are established campgrounds
+# It also assumes that the user does not want group camping. We are assuming accuracy of data from Ontario Geohub and the Algonquin
+# Park main website. 
+
+# Planned for limitations: 
+
+# Inputs and outputs: Input data will be _____, the campsites that fit the suitability criteria will be written to a csv file 
+
+# References: Ontario Geohub (URL), Ontario Parks (URL)
+
+# Contribution of team members to implementation: 
