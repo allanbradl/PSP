313697f9f0f2805fed355d14f7d9c6f7fcb7aa06
James Serendip <james.serendip@flemingcollege.ca>
2020-12-03 16:12:43 -0800

    fill dictionary with campground keys and field values

--- AlgCampSites1James.py
+++ AlgCampSites1James.py
@@ -1,14 +1,31 @@
 # AlgCampSites1
 
-# get connected to our geodatabase
+# import arcpy and get connected to our geodatabase
 import arcpy
 from arcpy import env
 
-env.workspace = r"C:\PSP\GroupProject\Algonquin\Algonquin\PSPgrp\CampgroundsData.gdb"
+arcpy.env.workspace = r"C:\PSP\GroupProject\Algonquin\Algonquin\PSPgrp\CampgroundsData.gdb"
 
+# set variable for feature class definition
+featureClasses = arcpy.ListFeatureClasses()
+campgrounds = featureClasses[0]
+fieldName = [f.name for f in arcpy.ListFields("Campgrounds")]  # sets field names to scan through later 
 
-fieldList = arcpy.ListFields("Campgrounds")
-for field in fieldList:
+# printing info to help view and construction ------ remove when ready!---------------
+# print(campgrounds)
+# print(" fields: " + str(fieldName))
+# print()
+# ----------------------------------------this can be removed for final, but leaving in in case we want to view it later----------
 
-     print(field.name)  
-print()
+# create empty dictionary for campsite info and empty string to hold values of fields for each site
+sitesdict = {}
+sitefields = []
+
+# loop through the feature class attribute table and assign evaluation criteria field values to each campsite (key)
+with arcpy.da.SearchCursor(campgrounds, fieldName) as cursor:
+    for row in cursor:
+         # print(row[6], row[18], row[21], row[22], row[17], row[12], row[20])
+         sitefields = [row[18], row[21], row[22], row[17], row[12], row[20]]
+         sitesdict = {row[6]: sitefields}
+         print(sitesdict)      
+
