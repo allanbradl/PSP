96dbd8f6b0bc0b3adee9ab5e3a545a488382ff84
kathrynlflemingc <kathryn.little@flemingcollege.ca>
2020-12-06 11:43:57 -0500

    program purpose

---
+++ AlgonquinCampgroundsWorking.py
@@ -0,0 +1,28 @@
+# GEOM 67 Group Project 
+# Name of Program: Alongquin Park Campground Selector
+# Authors: Kristine Luangkhot, James Serendip, Kathryn Little, Kendrick Lok 
+# Date last modified: 
+# Program Purpose: to conduct a site suitability analysis for a campground in Algonquin Provincial Park along the highway 60 corridor
+
+# Program Use: This program will be used by people looking to select a campground in Algonquin Park based on specific ranked criteria
+
+# Program Structure: 
+
+# Assumptions made: This program assumes that the user is only looking to book a campground along the Highway 60 corridor of Algonquin
+# Provincial Park in Ontario. It also assumes that elevation and ground level are not a factor, given these are established campgrounds
+# It also assumes that the user does not want group camping. We are assuming accuracy of data from Ontario Geohub and the Algonquin
+# Park main website. 
+
+# Planned for limitations: 
+
+# Inputs and outputs: Input data will be _____, the campsites that fit the suitability criteria will be written to a csv file 
+
+# References: Ontario Geohub (URL), Ontario Parks (URL)
+
+# Contribution of team members to implementation: 
+
+print("Welcome to the Algonquin Provincial Park Campground Selector.") 
+print("Algonquin offers a two different types of camping experiences:backcountry and drive-in camping in developed campgrounds.") 
+print("Along the Highway 60 corridor, there are 9 campgrounds containing 1,257 campsites.")
+print("In a park so large, it can be difficult to choose a campsite when making a reservation.") 
+print("This program should help make it a little bit easier by taking your preferences and choosing up to three campgrounds which meet those criterion.")
